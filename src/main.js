adf052031516a737a1c5e28eedbc961e81e40e4a
jahongir7 <jahongir95119@gmail.com>
2022-10-14 18:28:06 +0500

    added tailwind css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import router from "./routes";
+import "@/assets/css/styles.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import VueRouter from "vue-router";
 import Home from "./components/Home-Main.vue";
 
 const router = new VueRouter({
+  mode: "history",
   routes: [
     {
       path: "/",
